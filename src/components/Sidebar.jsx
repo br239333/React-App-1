a56f0a3fe823a26bba4c04e299136271f3acc78d
br239333 <br239333@ucf.edu>
2026-02-15 18:03:24 -0500

    WE COMMIT

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,121 @@
-import { useState, useCallback } from "react"
+import { useState } from "react"
 /**
  * Renders an array of strings passed in that can be filtered and added to as an
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
-  let [newMenuItem, setNewMenuItem] = useState("")
+//export default function Sidebar() {
+//im simply commenting this out and rewriting it so i can start learning the syntax a little bit better
+
+//THIS CODE SHOULD ACTUALLY BE THIS:
+//we define the sidebar component
+//this will pull out the user inputs value
+export default function Sidebar({userInput}){
+
+
+  //we create a state with useState
+  //The state is used to hold user input
+  let [menuItems,setMenuItems] = useState(userInput)
+
+  //This is the initial code again im just commenting it out but I would like to keep it here to look back at it if i get confused
+  //let [newMenuItem, setNewMenuItem] = useState("")
+
+
+
+ //instructions down here spacing the code out just because my eyes are horrible
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
-  let [filter, setFilter] = useState("")
+  // let [menuItems, setMenuItems] = useState(userInput)
+
+
+let [newMenuItem, setNewMenuItem] = useState("")
+
+
+let [filter, setFilter] = useState("")
+
+//i know i did not use a call back im still have a little difficulty with that but I still wanted to try to make this functional
+  //Im creating a function
+  //this function will excute once a user click the button
+  function addItemToMenu(){
+
+
+    //code will not execute if nothing is inputed
+    if(newMenuItem === ""){
+      return
+    }
+
+    //copy array
+    let newArr = []
+
+    //we loop over every item in the array until we are done
+    //once the loop is done we take each item (from position i) and put it in the new array NewArr
+    for(let i = 0; i < menuItems.length; i++){
+      newArr[i] = menuItems[i]
+    }
+    //we add the item to the end of the array
+    newArr.push(newMenuItem)
+
+    //we then update the menue item state
+    setMenuItems(newArr)
+    //this updates the input box and it clears automatically hence why we use an empty string
+    setNewMenuItem("")
+  }
+
+
+
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
-  let addMenuItem = useCallback(() => {
-    console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+  // let addMenuItem = useCallback(() => {
+  //   console.log("Added menu item")
+  // }, [])
+
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+
+
+
+  let displayMenuItems = []
+
+  //if the filter input box is equal to an empty string (no user input) it wont filter anything and it will display the menu items instead
+  //"else" we execute the next block of code if there is input in it
+  if(filter === ""){
+      displayMenuItems = menuItems
+
+  } else {
+    //so if the input box is not empty:
+    //first we make sure that input is all the same case this could cause an issue
+    //make a variable to hold values we make into lowercase
+      let lowerCaseFilter = filter.toLowerCase()
+
+      for (let i = 0; i < menuItems.length; i++) {
+
+
+        //we get the current item
+        //obv make a variable for that
+        let currentMenuItem = menuItems[i]
+        //makes the current menu item lowercase
+        let lowerCaseItem = currentMenuItem.toLowerCase()
+        //this will check if the now lowercaseItem contains the lowecase filter
+        //if it doesnt "!==" it returns back to that positon
+        if (lowerCaseItem.indexOf(lowerCaseFilter) !== -1) {
+
+          //then once its all done we use.push to add the item to the array to display
+          displayMenuItems.push(currentMenuItem)
+        }
+      }
+    }
+
 
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
+
+//Take the array displaymenuitems, and we make it into an array of list items!
+let listItems = displayMenuItems.map(function(menuInput, placement) {
+  //we store that
+  let iteminList = <li key={placement}>{menuInput}</li>
+  return iteminList
+})
+
+
   return (
     <div>
       <input
@@ -33,9 +126,8 @@ export default function Sidebar() {
       ></input>
       <br />
       <button
-        onClick={() => {
-          /* TODO: 3 */
-        }}
+
+        onClick={addItemToMenu}
       >
         Add Item
       </button>
@@ -47,6 +139,11 @@ export default function Sidebar() {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+
+      {/* then we display the items by refrencing the element above */}
+      <ul>
+        {listItems}
+      </ul>
     </div>
   )
 }
